e72732d916a75e1aaa57a8432b5d51da07bbfeba
Artyzzz <394075artem@gmail.com>
2025-11-07 15:50:44 +0300

    Stage 1

---
+++ stats/stats-dto/src/main/java/ru/practicum/ewm/stats/client/EndpointHit.java
@@ -0,0 +1,32 @@
+package ru.practicum.ewm.stats.client;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EndpointHit {
+    private Long id;
+
+    @NotBlank(message = "App cannot be blank")
+    private String app;
+
+    @NotBlank(message = "URI cannot be blank")
+    private String uri;
+
+    @NotBlank(message = "IP cannot be blank")
+    private String ip;
+
+    @NotNull(message = "Timestamp cannot be null")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime timestamp;
+}

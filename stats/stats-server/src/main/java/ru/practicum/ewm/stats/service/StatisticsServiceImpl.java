42a9a16d0244d948de3ea0e6f519b2cd8cc2dae0
Artyzzz <394075artem@gmail.com>
2025-11-11 14:34:48 +0300

    Stage 2

--- stats/stats-server/src/main/java/ru/practicum/ewm/stats/service/StatisticsServiceImpl.java
+++ stats/stats-server/src/main/java/ru/practicum/ewm/stats/service/StatisticsServiceImpl.java
@@ -2,8 +2,10 @@ package ru.practicum.ewm.stats.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 import ru.practicum.ewm.stats.client.EndpointHit;
 import ru.practicum.ewm.stats.client.ViewStats;
 import ru.practicum.ewm.stats.mapper.StatisticsMapper;
@@ -54,8 +56,14 @@ public class StatisticsServiceImpl implements StatisticsService {
     }
 
     private void validateTimeRange(LocalDateTime start, LocalDateTime end) {
+        if (start == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Start time cannot be null");
+        }
+        if (end == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "End time cannot be null");
+        }
         if (start.isAfter(end)) {
-            throw new IllegalArgumentException("Start time must be before end time");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Start time must be before end time");
         }
     }
 }
